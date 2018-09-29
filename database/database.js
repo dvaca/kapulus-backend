9b2690bd4ca3ec5895b39499449fcbb1a17618c4
David Vaca <david1986@gmail.com>
2018-09-29 17:21:49 -0500

    Conectado a bd en heroku

--- database/database.js
+++ database/database.js
@@ -3,9 +3,11 @@ var pg = require("pg");
 const { Pool } = require('pg')
 
 var config = {
-  user: 'postgres',
-  database: 'ManageEvents', 
-  password: 'postgres123', 
+	
+  host: 'ec2-50-16-196-57.compute-1.amazonaws.com',
+  user: 'klpsdnbujsrqfh',
+  database: 'd7tjn0c1k927pq', 
+  password: '32069e24b8717feceb9544868a8615ce18bb93cafab653fd910da71e034236fc', 
   port: 5432, 
   max: 10, // max number of connection can be open to database
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed

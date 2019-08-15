a6bb4950f648fd835b42a230629e7787694987e3
dvaca <43690454+dvaca@users.noreply.github.com>
2019-08-15 17:29:28 -0500

    mailing

--- modulos/registro.js
+++ modulos/registro.js
@@ -928,7 +928,7 @@ app.get('/asistente/:idevento', (req, res, next) => {
 		TRANSPORT = {
 		  //service: 'Gmail', 
 		  host: 'mail.congresodeetica.com.co',
-		  port: 465,
+		  port: 993,
 		  secure: false,
 		  auth: { 
 		  //user: 'contacto@kapulusinternational.com',

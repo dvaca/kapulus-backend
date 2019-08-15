29706ea7eafcdcea9eb315edb8b0d40a685f5c52
dvaca <43690454+dvaca@users.noreply.github.com>
2019-08-15 17:02:42 -0500

    Ajuste mailing copnia

--- modulos/registro.js
+++ modulos/registro.js
@@ -928,7 +928,7 @@ app.get('/asistente/:idevento', (req, res, next) => {
 		TRANSPORT = {
 		  //service: 'Gmail', 
 		  host: 'mail.congresodeetica.com.co',
-		  port: 465,
+		  port: 26,
 		  secure: false,
 		  auth: { 
 		  //user: 'contacto@kapulusinternational.com',

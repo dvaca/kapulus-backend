f9cd8a00152ef20a8f23ff9e75b20853ae9b752a
dvaca <43690454+dvaca@users.noreply.github.com>
2021-11-02 13:19:09 -0500

    2021

--- database/database.js
+++ database/database.js
@@ -22,7 +22,7 @@ var config = {
   //password: 'MrPotato*',
   port: 5432,
   max: 10, // max number of connection can be open to database
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 };
 
 const pool = new Pool(config);
